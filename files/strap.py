48f7f09102e6c226bf483bcccecfae4c149e6f18
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:05:32 +0300

    copyfile

--- files/strap.py
+++ files/strap.py
@@ -1,4 +1,4 @@
 for i in ["ftpd.lja", "ftpd.py"]:
-    shutil.copy(i, path.join(root, "bin", i))
-shutil.copy("ftp_serv.py", path.join(root, "..", "ftp_serv.py"))
-shutil.copy("ftp_server.mpy", path.join(root, "lib", "ftp_server.mpy"))
+    shutil.copyfile(i, path.join(root, "bin", i))
+shutil.copyfile("ftp_serv.py", path.join(root, "..", "ftp_serv.py"))
+shutil.copyfile("ftp_server.mpy", path.join(root, "lib", "ftp_server.mpy"))
